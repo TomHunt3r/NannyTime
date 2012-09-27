6ccab7938b375a3cbf0fdcc210145355b1697932
jumahe <jumahe@gmail.com>
2012-09-27 10:25:56 +0200

    conf update

--- conf/conf.php
+++ conf/conf.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_HOST","db669.1and1.fr");
-define("DB_USER","dbo176957915");
-define("DB_PASS","4WBYZ6Jt");
-define("DB_BASE","db176957915");
+define("DB_HOST","");
+define("DB_USER","");
+define("DB_PASS","");
+define("DB_BASE","");
 ?>
